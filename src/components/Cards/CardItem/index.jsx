ec19b759ab483063bf7df4061b44b8f9e35eb55e
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-29 15:25:07 +0700

    Create CardItem

--- src/components/Cards/CardItem/index.jsx
+++ src/components/Cards/CardItem/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
-const CardItem = () => {
+const CardItem = ({ src, text, label, path }) => {
 	return (
-		<div>
-			<h2>CardItem</h2>
-		</div>
+		<>
+			<li className="cards__item">
+				<Link to={path} className="cards__item__link">
+					<figure className="cards__item__pic-wrap" data-category={label}>
+						<img src={src} alt="Travel" className="cards__item__img" />
+					</figure>
+					<div className="cards__item__info">
+						<h5 className="cards__item__text">{text}</h5>
+					</div>
+				</Link>
+			</li>
+		</>
 	);
 };
 
