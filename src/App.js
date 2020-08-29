48061ccc6b093e9fcf06407e3da369cb9af05850
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-29 15:24:06 +0700

    Update Route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import NavBar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const Home = React.lazy(() => import("./components/pages/Home"));
+const Services = React.lazy(() => import("./components/pages/Services"));
+const Products = React.lazy(() => import("./components/pages/Products"));
+const SignUp = React.lazy(() => import("./components/pages/SignUp"));
+const NotFound = React.lazy(() => import("./components/NotFound"));
 
 function App() {
 	return (
@@ -12,6 +16,10 @@ function App() {
 				<NavBar />
 				<Switch>
 					<Route path="/" exact component={Home} />
+					<Route path="/services" component={Services} />
+					<Route path="/sign-up" component={SignUp} />
+					<Route path="/products" component={Products} />
+					<Route component={NotFound} />
 				</Switch>
 			</Router>
 		</Suspense>
