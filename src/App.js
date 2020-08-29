a24372d6e3f6f2270bc5547d1b8c0d739a9a135f
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-29 12:19:22 +0700

    Initial Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./components/Navbar";
 
 function App() {
-	return <div className="app">Hello world</div>;
+	return (
+		<div className="app">
+			<NavBar />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+	return (
+		<div>
+			<H2>nav bar</H2>
+		</div>
+	);
+};
+
+export default NavBar;
