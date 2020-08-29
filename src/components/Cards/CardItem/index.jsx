6b41b869548614dc51483306ddcac7837abdc7e7
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-29 14:36:22 +0700

    Cards component initial

---
+++ src/components/Cards/CardItem/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+const CardItem = () => {
+	return (
+		<div>
+			<h2>CardItem</h2>
+		</div>
+	);
+};
+
+export default CardItem;
---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+const Cards = () => {
+	return (
+		<div>
+			<h2>Cards</h2>
+		</div>
+	);
+};
+
+export default Cards;
