dab5897641a716570dd44976f7668c8ff293c7e4
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-29 15:25:15 +0700

    cards component

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,9 +1,42 @@
 import React from "react";
 import "./style.css";
-const Cards = () => {
+import CardItem from "./CardItem";
+const Cards = ({ blogsLarge, blogsMedium }) => {
 	return (
-		<div>
-			<h2>Cards</h2>
+		<div className="cards">
+			<h1>Check out these EPIC Destinations!</h1>
+			<div className="cards__container">
+				<div className="cards__wrapper">
+					<ul className="cards__items">
+						{blogsLarge.map((blog) => {
+							return (
+								<CardItem
+									key={blog.id}
+									id={blog.id}
+									src={blog.src}
+									text={blog.text}
+									label={blog.label}
+									path={blog.path}
+								/>
+							);
+						})}
+					</ul>
+					<ul className="cards__items">
+						{blogsMedium.map((blog) => {
+							return (
+								<CardItem
+									key={blog.id}
+									id={blog.id}
+									src={blog.src}
+									text={blog.text}
+									label={blog.label}
+									path={blog.path}
+								/>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
