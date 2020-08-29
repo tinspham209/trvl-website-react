e2a098b80358c2613e5a1c43bcaaa64b9eb73f24
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-29 14:22:00 +0700

    remove div Hoc

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -19,16 +19,14 @@ export const Button = ({
 
 	const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 	return (
-		<div>
-			<Link to="/sign-up" className="btn-mobile">
-				<button
-					className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-					onClick={onClick}
-					type={type}
-				>
-					{children}
-				</button>
-			</Link>
-		</div>
+		<Link to="/sign-up" className="btn-mobile">
+			<button
+				className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+				onClick={onClick}
+				type={type}
+			>
+				{children}
+			</button>
+		</Link>
 	);
 };
