6d46161b0b5d2bcca7d45c6a82789aa2c28d86a7
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-29 13:31:10 +0700

    add Homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import NavBar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+const Home = React.lazy(() => import("./components/pages/Home"));
+
 function App() {
 	return (
-		<>
+		<Suspense fallback={<div>Loading...</div>}>
 			<Router>
 				<NavBar />
 				<Switch>
-					<Route path="/" exact />
+					<Route path="/" exact component={Home} />
 				</Switch>
 			</Router>
-		</>
+		</Suspense>
 	);
 }
 
